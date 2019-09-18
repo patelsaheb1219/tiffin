4284d8894f8e3e36bba9e0ee4b87a8141d801336
patelsaheb1219 <kishan.patel@eccocar.com>
2019-09-18 10:03:45 +0530

    added tiffinCount component

--- src/TiffinCount.js
+++ src/TiffinCount.js
@@ -1,12 +1,67 @@
 import React, {Component} from 'react';
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View, Image, AsyncStorage } from "react-native";
+import { Title, Text, TextInput, Subheading, Button } from "react-native-paper";
 import Header from "./Header";
 
 class TiffinCount extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            tiffinNumber: null
+        }
+    }
+
     render() {
+        const { navigation } = this.props;
+        const title = navigation.getParam('title');
+        const subtitle = navigation.getParam('subtitle');
+        const amount = navigation.getParam('amount');
+        const showImage = navigation.getParam('showImage');
         return (
             <SafeAreaView>
-                <Header title="Tiffin Count" subtitle="" showBackArrow={true} />
+                <Header title="Tiffin Count" subtitle="" showBackArrow={true} navigation={navigation} />
+                <View style={{flexGrow:1}}/>
+                <View style={{ flexGrow:1, alignItems: 'center', marginVertical: 20 }} >
+                    <Title>{title}</Title>
+                    <Subheading>{subtitle}</Subheading>
+                </View>
+                <View style={{ flexGrow:1, alignItems: 'center', marginVertical: 20 }}>
+                    { showImage === 'lunch' ? <Image source={require('../assets/lunch.jpeg')} /> : null }
+                    { showImage === 'dinner' ? <Image source={require('../assets/dinner.jpeg')} /> : null }
+                    { showImage === 'breakfast' ? <Image source={require('../assets/breakfast.jpeg')} /> : null }
+                </View>
+                <View>
+                    <TextInput
+                        label='Amount'
+                        value={amount.toString()}
+                        disabled
+                        mode='outlined'
+                        style={{ marginHorizontal: 10 }}
+                    />
+                </View>
+                <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ flexGrow: 1 }}>
+                            <Button mode="contained" onPress={() => console.log('Pressed')}>
+                                -
+                            </Button>
+                        </View>
+                        <View style={{ flexGrow: 2 }}>
+                            <TextInput
+                            value={this.state.tiffinNumber ? this.state.tiffinNumber : 0}
+                            mode='outlined'
+                            disabled
+                            style={{ marginHorizontal: 10 }}
+                        />
+                        </View>
+                        <View style={{ flexGrow: 1 }}>
+                            <Button mode="contained" onPress={() => console.log('Pressed')}>
+                                +
+                            </Button>
+                        </View>
+                    </View>
+                </View>
             </SafeAreaView>
         );
     }
--- src/TiffinMakerList.js
+++ src/TiffinMakerList.js
@@ -14,19 +14,60 @@ class TiffinMakerList extends Component {
                     description="Lunch"
                     left={props => <List.Icon {...props} icon="person" />}
                     right={props => <List.Icon {...props} icon="arrow-forward"  />}
-                    onPress={() => { this.props.navigation.navigate('TiffinCount') }}
+                    onPress={
+                        () => { this.props.navigation.navigate('TiffinCount' , {
+                            title: 'Aman Shah',
+                            subtitle: 'Lunch',
+                            amount: 50,
+                            navigation: this.props.navigation,
+                            showImage: 'lunch'
+                        })
+                     }}
+                />
+                <List.Item
+                    title="Sangita Ben"
+                    description="Breakfast"
+                    left={props => <List.Icon {...props} icon="person" />}
+                    right={props => <List.Icon {...props} icon="arrow-forward" />}
+                    onPress={
+                        () => { this.props.navigation.navigate('TiffinCount' , {
+                            title: 'Sangita Ben',
+                            subtitle: 'Breakfast',
+                            amount: 30,
+                            navigation: this.props.navigation,
+                            showImage: 'breakfast'
+                        })
+                        }}
                 />
                 <List.Item
                     title="Sangita Ben"
                     description="Lunch"
                     left={props => <List.Icon {...props} icon="person" />}
                     right={props => <List.Icon {...props} icon="arrow-forward" />}
+                    onPress={
+                        () => { this.props.navigation.navigate('TiffinCount' , {
+                            title: 'Sangita Ben',
+                            subtitle: 'Lunch',
+                            amount: 70,
+                            navigation: this.props.navigation,
+                            showImage: 'lunch'
+                        })
+                        }}
                 />
                 <List.Item
                     title="Sangita Ben"
                     description="Dinner"
                     left={props => <List.Icon {...props} icon="person" />}
                     right={props => <List.Icon {...props} icon="arrow-forward" />}
+                    onPress={
+                        () => { this.props.navigation.navigate('TiffinCount' , {
+                            title: 'Sangita Ben',
+                            subtitle: 'Dinner',
+                            amount: 60,
+                            navigation: this.props.navigation,
+                            showImage: 'dinner'
+                        })
+                        }}
                 />
             </SafeAreaView>
         );
